5b98448fa3191c26a425af15b5d4d8e604cdbbeb
u <s03@protonmail.com>
2024-05-30 14:47:24 +0200

    finished testing chapter

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 #![no_std]      // don't link the Rust std lib 
 #![no_main]     // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use core::convert::TryInto;
 use core::panic::PanicInfo;
-
-
-mod vga_buffer;
-
-
-static HELLO: &[u8] = b"Hello World!";
+use blog_os::println;
 
 
 
 // This function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -20,10 +18,22 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 
+// panic handler for test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
+    loop {}
+}
+
 
 #[no_mangle]    // don't mangle the _start name function
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
-    panic!("Intentional triggering the panic handler...");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
+
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,7 +1,6 @@
 
 use volatile::Volatile;
 use core::fmt;
-use core::fmt::Write;
 use lazy_static::lazy_static;
 use spin::Mutex;
 
@@ -161,15 +160,12 @@ pub fn _print(args: fmt::Arguments) {
 }
 
 
-
-pub fn print_something() {
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
